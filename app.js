dbab958fda23cb4aa03a6ebc14784d9665fade9c
Ruud <rburger@totalactivemedia.nl>
2013-05-22 16:10:38 +0200

    Graceful restart

--- app.js
+++ app.js
@@ -23,23 +23,51 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.compress());
 
+// Replace header
 app.use(function(req, res, next){
 	res.setHeader('X-Powered-By', 'CouchPotato ('+app.get('port')+')');
 	next();
 });
 
-// development only
+// Development only
 if(app.get('env') == 'development') {
 	app.use(express.errorHandler());
 	app.use(express.logger('dev'));
 }
 
+// Don't accept new incoming connections when starting
+var shutting_down = false;
+app.use(function(req, res, next) {
+	if (!shutting_down)
+		return next();
+	res.setHeader('Connection', 'close');
+	return res.send(502, 'Server is in the process of restarting');
+});
+
+// Route
 app.get('/', home.index);
 app.get('/eta/tt:imdb(\\d+)', eta.imdb);
 app.get('/ismovie/tt:imdb(\\d+)', ismovie.imdb);
 app.get('/info/tt:imdb(\\d+)', info.imdb);
 app.get('/search/:query', search.query);
 
-http.createServer(app).listen(app.get('port'), function() {
+httpServer = http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Graceful shutdown
+process.on('SIGTERM', function() {
+	console.log('Received kill signal (SIGTERM), shutting down gracefully.');
+	shutting_down = true;
+
+	httpServer.close(function() {
+		console.log('Closed out remaining connections.');
+		return process.exit();
+	});
+
+	return setTimeout(function() {
+		console.error('Could not close connections in time, forcefully shutting down');
+		return process.exit(1);
+	}, 30 * 1000);
+});
+
