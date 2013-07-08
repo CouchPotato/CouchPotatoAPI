f77a6368ecd4e54e04c0acce0ad4329fc716fff8
Ruud <rburger@totalactivemedia.nl>
2013-07-08 12:12:27 +0200

    Make sure a valid url is send back

--- routes/updater.js
+++ routes/updater.js
@@ -1,14 +1,24 @@
 /**
  * Return the url source updater can find the zip
  */
+var correct_reg = /^([a-z]+)$/i;
+var isAlphabetOnly = function(str){
+    return str.match(correct_reg);
+}
+
 exports.url = function(req, res) {
+
+	// Don't let packagers screw up requests
+	var repo = req.query.repo && isAlphabetOnly(req.query.repo) ? req.query.repo : 'RuudBurger',
+		name = req.query.name && isAlphabetOnly(req.query.name) ? req.query.name : 'CouchPotatoServer',
+		branch = req.query.branch && isAlphabetOnly(req.query.branch) ? req.query.branch : 'master';
 
 	res.type('application/json');
 	res.json({
 		'url': 'https://codeload.github.com/'+
-			(req.query.repo || 'RuudBurger')+'/'+
-			(req.query.name || 'CouchPotatoServer')+'/zip/'+
-			(req.query.branch || 'master'),
+			repo+'/'+
+			name+'/zip/'+
+			branch,
 		'type': 'zip'
 	});
 
