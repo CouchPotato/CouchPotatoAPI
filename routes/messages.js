ae6fca615c8aa579c8b72b3d692cefd0e7e25ae9
Ruud <ruud@crashdummy.nl>
2013-10-18 12:59:41 +0200

    Remove support notification again..

--- routes/messages.js
+++ routes/messages.js
@@ -10,13 +10,7 @@ exports.list = function(req, res) {
 			'time': strtotime('2013-05-02 15:00'),
 			'important': true,
 			'message': 'If you read this, you are awesome. Thanks for using CouchPotato!',
-		},
-		{
-			'id': 3,
-			'time': strtotime('2013-10-12 20:00'),
-			'important': true,
-			'message': 'New donation options are available on <a href="https://couchpota.to/support/" target="_blank">couchpota.to</a>. Just saying ;)',
-		},
+		}
 	]);
 
 };
