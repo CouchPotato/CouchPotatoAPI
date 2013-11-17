0bb601fda096e198ef49b5973a3124b6c235261d
Ruud <ruud@crashdummy.nl>
2013-11-17 19:26:20 +0100

    Update message

--- routes/messages.js
+++ routes/messages.js
@@ -6,10 +6,10 @@ exports.list = function(req, res) {
 	res.type('application/json');
 	res.json([
 		{
-			'id': 1,
-			'time': strtotime('2013-05-02 15:00'),
+			'id': 5,
+			'time': strtotime('2013-11-17 19:30'),
 			'important': true,
-			'message': 'If you read this, you are awesome. Thanks for using CouchPotato!',
+			'message': 'Go checkout the new browser extensions for <a href="https://addons.mozilla.org/en-US/firefox/addon/couchpotato/" target="_blank">Firefox</a> and <a href="https://chrome.google.com/webstore/detail/couchpotato/jochingjncojldfclaicaomboafaiong" target="_blank">Chrome</a>! Will make your ass a lot fatter ;)',
 		}
 	]);
 
