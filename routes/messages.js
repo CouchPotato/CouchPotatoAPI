4ac43825a7c4f54ab01d5567d6e1d943cff6cf71
Ruud <ruud@crashdummy.nl>
2013-11-22 15:06:19 +0100

    Load messages from data dir

--- routes/messages.js
+++ routes/messages.js
@@ -1,16 +1,13 @@
+var fs = require('fs');
+
 /**
  * Return messages
  */
 exports.list = function(req, res) {
 
+	var messages = JSON.parse(fs.readFileSync('./data/messages.json').toString());
+
 	res.type('application/json');
-	res.json([
-		{
-			'id': 5,
-			'time': strtotime('2013-11-17 19:30'),
-			'important': true,
-			'message': 'Go checkout the new browser extensions for <a href="https://addons.mozilla.org/en-US/firefox/addon/couchpotato/" target="_blank">Firefox</a> and <a href="https://chrome.google.com/webstore/detail/couchpotato/jochingjncojldfclaicaomboafaiong" target="_blank">Chrome</a>! Will make your ass a lot fatter ;)',
-		}
-	]);
+	res.json(messages);
 
 };
