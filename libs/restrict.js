855b6031553540fb9af688a3b7c0f4f0151c96db
Ruud <ruud@crashdummy.nl>
2014-10-07 20:37:06 +0200

    Remove logging

--- libs/restrict.js
+++ libs/restrict.js
@@ -1,22 +1,23 @@
-var log = global.createLogger(__filename)
+var version_error = 'Unsupported version of CP, please update',
+	not_found = 'Not found';
 
 exports.restrict = function(req, res, next) {
 
 	if(!isWhitelisted(req.ip)){
 		var stats = req.stats || {};
 		if(!stats.os || !stats.type || !stats.version){
-			log.error('404', {
-				'url': req.originalUrl,
-				'version': req.version,
-				'api_version': req.api_version,
-				'user': req.user,
-				'stats': req.stats
-			})
-			res.status(404).send('Not found');
+//			log.error('404', {
+//				'url': req.originalUrl,
+//				'version': req.version,
+//				'api_version': req.api_version,
+//				'user': req.user,
+//				'stats': req.stats
+//			})
+			res.status(404).send(req.user || req.api_version > 0 ? version_error : not_found);
 			return;
 		}
 		else if (req.api_version === 0 || req.identifier === null){
-			res.status(401).send('Failed because you are using an unsupported version of CP, please update');
+			res.status(401).send(version_error);
 			return;
 		}
 	}
