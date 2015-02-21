65d06fa55c6f383f9e5dcc8346bc4a6a71490699
Ruud <ruud@crashdummy.nl>
2015-02-21 13:28:36 +0100

    Use current dir to find data folder

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,10 @@
-var fs = require('fs');
+var fs = require('fs'),
+	path = require('path');
 
 /**
  * Return messages
  */
-var messages = JSON.parse(fs.readFileSync('./data/messages.json').toString()),
+var messages = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/messages.json')).toString()),
     legacy_message = {
         "time": 1400766111,
         "message": "It looks like you're running an outdated CouchPotato. Please update, be awesome.",
